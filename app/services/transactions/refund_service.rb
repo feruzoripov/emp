c493ec8e21084d3d14f3effa4b60411d610cda0e
Feruz Oripov <feruzoripov@protonmail.com>
2022-08-03 22:37:49 +0500

    fixing tabs

--- app/services/transactions/refund_service.rb
+++ app/services/transactions/refund_service.rb
@@ -1,29 +1,29 @@
 module Transactions
-	class RefundService < BaseService
+  class RefundService < BaseService
 
-		def initialize(merchant_id:, amount:, uuid:)
-			super
-			@amount = amount
-			@uuid = uuid
-		end
+    def initialize(merchant_id:, amount:, uuid:)
+      super
+      @amount = amount
+      @uuid = uuid
+    end
 
-		def run
-			if valid_to_refund?
-				Transaction::Refund.create!(parent_transaction: charged_transaction, amount: amount)
-				charged_transaction.refunded!
-			else
-				Transaction::Refund.create!(amount: amount, status: :error)
-			end
-		end
+    def run
+      if valid_to_refund?
+        Transaction::Refund.create!(parent_transaction: charged_transaction, amount: amount)
+        charged_transaction.refunded!
+      else
+        Transaction::Refund.create!(amount: amount, status: :error)
+      end
+    end
 
-		private
+    private
 
-		def charged_transaction
-			@charged_transaction ||= Transaction::Charged.find(uuid)
-		end
+    def charged_transaction
+      @charged_transaction ||= Transaction::Charged.find(uuid)
+    end
 
-		def valid_to_refund?
-			charged_transaction && charged_transaction.approved?
-		end
-	end
+    def valid_to_refund?
+      charged_transaction && charged_transaction.approved?
+    end
+  end
 end
