be7487e7d22430077c58092ae86b3edaefcdbd9f
Feruz Oripov <feruzoripov@protonmail.com>
2022-08-03 23:07:38 +0500

    implementing authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  def status
+    render json: :ok
+  end
+
+  private
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JwtService.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
