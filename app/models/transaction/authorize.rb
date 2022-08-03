813d97aa330bb9336e534f27ef5e1a8a30c38063
Feruz Oripov <feruzoripov@protonmail.com>
2022-08-04 00:08:57 +0500

    validates

--- app/models/transaction/authorize.rb
+++ app/models/transaction/authorize.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class Transaction::Authorize < Transaction
-  validates :customer_email, presence: true
-  validates :customer_email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :amount, presence: true, numericality: { greater_than: 0 }
 end
