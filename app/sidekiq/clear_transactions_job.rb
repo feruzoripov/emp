d74b6a93b009ef2d9bb1187db92e3559be54aaf7
Feruz Oripov <feruzoripov@protonmail.com>
2022-08-04 11:55:51 +0500

    fix styling

--- app/sidekiq/clear_transactions_job.rb
+++ app/sidekiq/clear_transactions_job.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class ClearTransactionsJob
   include Sidekiq::Job
 
-  def perform(*args)
+  def perform(*_args)
     transactions = Transaction.where('created_at < ?', 1.hour.ago)
     puts '##########################################'
     puts '# Deleting transactions created 1 hour ago'
